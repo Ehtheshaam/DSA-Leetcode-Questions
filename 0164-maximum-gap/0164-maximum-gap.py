9c86062c17ea25b7a79f5d4989288cfa2e9e682b
Ehtheshaam <skehtheshaam2006@gmail.com>
2026-08-21 14:11:53 +0530

    Time: 253 ms (56.3%), Space: 22 MB (67.66%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maximumGap(self, nums):
+        max_gap = 0
+        nums.sort()
+        for i in range(len(nums)):
+            max_gap = max(max_gap,nums[i]-nums[i-1])
+        return max_gap    
