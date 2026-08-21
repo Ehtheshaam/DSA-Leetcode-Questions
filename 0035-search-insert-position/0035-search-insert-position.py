89ad515a00540a97414d1281d7fc93bc6fdfb210
Ehtheshaam <skehtheshaam2006@gmail.com>
2026-08-21 11:36:10 +0530

    Time: 0 ms (100%), Space: 12.8 MB (66.22%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -2,9 +2,7 @@ class Solution(object):
     def searchInsert(self, nums, target):
 
         for i in range(len(nums)):
-            if nums[i] == target:
+            if nums[i] >= target:
                 return i
-        for i in range(len(nums)):
-            if nums[i] > target:
-                return i
+
         return len(nums)
