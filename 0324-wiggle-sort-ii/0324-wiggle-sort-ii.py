3e60b3437b9df286ee5df559214197232297b1be
Ehtheshaam <skehtheshaam2006@gmail.com>
2026-08-24 10:51:21 +0530

    Time: 14 ms (28.2%), Space: 14 MB (45.06%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -13,5 +13,5 @@ class Solution(object):
                 result.append(nums[right])
                 right -= 1
 
-        nums[:] = result        
-        
+        for i in range(n):
+            nums[i] = result[i]
