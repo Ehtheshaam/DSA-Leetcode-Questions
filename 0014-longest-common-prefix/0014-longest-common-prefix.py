3cce1abcdb9e13339c17ef3ff26cf30ccdc694c0
Ehtheshaam <skehtheshaam2006@gmail.com>
2026-08-25 09:04:39 +0530

    Time: 0 ms (100%), Space: 12.5 MB (34.22%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -7,3 +7,13 @@ class Solution(object):
                 res = res[:-1]
 
         return res
+
+
+        # res = ""
+        # for i in range(len(strs[0])):
+        #     ch = strs[0][i]
+        #     for s in strs[1:]:
+        #         if i >= len(s) or s[i] != ch:
+        #             return res
+        #     res += ch
+        # return res
